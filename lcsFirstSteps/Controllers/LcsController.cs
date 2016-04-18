4e4243ece2e418ebdeb175cb8309b5a0ac1f79fd
lcsmungret <lcsmungret@bernas.it>
2016-04-18 10:41:18 +0100

    first model

---
+++ lcsFirstSteps/Controllers/LcsController.cs
@@ -0,0 +1,27 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------
+// <copyright file="LcsController.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The lcs controller.
+// </summary>
+// ------------------------------------------------------------------------------------------------------------------------
+namespace lcsFirstSteps.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    using lcsFirstSteps.Models;
+
+    /// <summary>The lcs controller.</summary>
+    public class LcsController : Controller
+    {
+        // GET: Lcs
+        /// <summary>The index.</summary>
+        /// <returns>The <see cref="ActionResult" />.</returns>
+        public ActionResult Index()
+        {
+            return View(new Person { Age = 15, DateOfBirth = new DateTime(2001, 1, 2, 14, 43, 00), FirstName = "FirstName", LastName = "LastName" });
+        }
+    }
+}
---
+++ lcsFirstSteps/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------
+// <copyright file="Person.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The person view model.
+// </summary>
+// ------------------------------------------------------------------------------------------------------------------------
+namespace lcsFirstSteps.Models
+{
+    using System;
+
+    /// <summary>The person view model.</summary>
+    public class Person
+    {
+        /// <summary>Gets or sets the age.</summary>
+        public int Age { get; set; }
+
+        /// <summary>Gets or sets the date of birth.</summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>Gets or sets the first name.</summary>
+        public string FirstName { get; set; }
+
+        /// <summary>Gets or sets the last name.</summary>
+        public string LastName { get; set; }
+    }
+}
