96e90ffa1a98235d160037eb967712f4ca289ac3
profesor <git@bernas.it>
2016-04-25 11:54:21 +0100

    added a form input

--- lcsFirstSteps/Controllers/LcsController.cs
+++ lcsFirstSteps/Controllers/LcsController.cs
@@ -23,5 +23,18 @@ namespace lcsFirstSteps.Controllers
         {
             return View(new Person { Age = 15, DateOfBirth = new DateTime(2001, 1, 2, 14, 43, 00), FirstName = "FirstName", LastName = "LastName" });
         }
+
+        public ActionResult Display(Person model)
+        {
+            var data = model ??
+                       new Person
+                       {
+                           Age = 15,
+                           DateOfBirth = new DateTime(2001, 1, 2, 14, 43, 00),
+                           FirstName = "Empty data",
+                           LastName = "Empty Data"
+                       };
+            return View(data);
+        }
     }
 }
